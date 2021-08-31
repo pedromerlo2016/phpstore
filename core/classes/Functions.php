d665817515cc0c100766dfb126d37f06f0d0aeb2
pedromerlo2016 <pedro_merlo@globo.com>
2021-08-31 17:30:25 -0300

    Aula007

--- core/classes/Functions.php
+++ core/classes/Functions.php
@@ -2,9 +2,25 @@
 
 namespace core\classes;
 
+use Exception;
+
 class Functions{
 
-    public function teste(){
-        echo 'Olá'; 
+    //============================================================
+    public static function Layout($estruturas , $dados=null){
+        // verifica se estrutura é um array
+        if(!is_array($estruturas)){
+            throw new Exception("Coleção de estruturas inválidas");
+        }
+        // variáveis
+        if(!empty($dados)&& is_array($dados)){
+            extract($dados);
+        }
+
+        // apresentar as views da aplicação
+        foreach($estruturas as $estrutura){
+            include("../core/views/$estrutura.php");
+        }
     }
+
 }
--- core/controladores/Main.php
+++ core/controladores/Main.php
@@ -2,12 +2,26 @@
 
 namespace core\controladores;
 
+use core\classes\Functions;
+
 class Main{
-
+    //============================================================
     public function index(){
-        echo "MAIN!!!";
-    }   
+        $clientes  =  ['Pedro', 'Paulo', 'Ana','João'];
+        
+        $dados=[
+            'titulo'=>'Este é título da minha página',
+            'clientes'  =>  ['Pedro', 'Paulo', 'Ana','João']
+        ];
 
+        Functions::Layout([
+            'layouts/html_header',
+            'pagina_inicial',
+            'layouts/html_footer',
+        ], $dados);
+
+    }   
+    //============================================================
     public function loja(){
         echo "Loja!!!";
     }   
