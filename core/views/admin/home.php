14490bcb027a7a2734f1d691c93827baf9473ff1
pedromerlo2016 <pedro_merlo@globo.com>
2021-09-21 15:08:44 -0300

    Aula 073

--- core/views/admin/home.php
+++ core/views/admin/home.php
@@ -1,3 +1,9 @@
-<div>
-    <h3>ADMIN</h3>
+<div class="contanier-fluid">
+    <div class="row mt-3">
+        <div class="col-md-3">
+        <?php include( __DIR__.'/layouts/admin_menu.php') ?>   
+
+        </div>
+        <div class="col-md-9">[conteudo]</div>
+    </div>
 </div>
---
+++ core/views/admin/layouts/admin_menu.php
@@ -0,0 +1,11 @@
+<div>
+    <div class="mb-3">
+        <a href="#">Início</a>
+    </div>
+    <div class="mb-3">
+        <a href="#">Clientes</a>
+    </div>
+    <div class="mb-3">
+        <a href="#">Encomendas</a>
+    </div>
+</div>
