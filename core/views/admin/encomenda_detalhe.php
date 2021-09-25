8716b148c2687ae0f3875909c563691a87b36d2e
pedromerlo2016 <pedro_merlo@globo.com>
2021-09-25 12:48:56 -0300

    Aula 092

--- core/views/admin/encomenda_detalhe.php
+++ core/views/admin/encomenda_detalhe.php
@@ -4,7 +4,17 @@
             <?php include(__DIR__ . '/layouts/admin_menu.php') ?>
         </div>
         <div class="col-md-10">
-            <h4>Detalhe da encomenda</h4><span><small><?= $encomenda->codigo_encomenda ?></small></span>
+            <div class="row">
+                <div class="col">
+                    <h4>Detalhe da encomenda</h4><span><small><?= $encomenda->codigo_encomenda ?></small></span>
+                </div>
+                <div class="col text-end">
+                    <div class="text-center p-3 badge bg-primary status-clicavel" onclick="apresentarModal()">
+                        <?= $encomenda->status ?>
+                    </div>
+                </div>
+
+            </div>
             <hr>
             <div class="row">
                 <div class="col">
@@ -28,15 +38,43 @@
                     </tr>
                 </thead>
                 <tbody>
-                       <?php foreach($lista_produtos as $produto): ?>
+                    <?php foreach ($lista_produtos as $produto) : ?>
                         <tr>
-                            <td><?=$produto->descricao_produto ?></td>
-                            <td>R$ <?=number_format($produto->preco_unidade,2,',','.') ?></td>
-                            <td class="text-center"><?=$produto->quantidade ?></td>
+                            <td><?= $produto->descricao_produto ?></td>
+                            <td>R$ <?= number_format($produto->preco_unidade, 2, ',', '.') ?></td>
+                            <td class="text-center"><?= $produto->quantidade ?></td>
                         </tr>
-                       <?php endforeach;?>
+                    <?php endforeach; ?>
                 </tbody>
             </table>
         </div>
     </div>
 </div>
+<!-- Modal -->
+<div class="modal fade" id="modalStatus" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Alterar status da encomenda</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <a href="#">Status</a><br>
+        <a href="#">Status</a><br>
+        <a href="#">Status</a><br>
+        <a href="#">Status</a><br>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        <!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+<script>
+    function apresentarModal(){
+        var modalStatus = new bootstrap.Modal(document.getElementById('modalStatus'));
+        modalStatus.show();
+    }
+</script>
